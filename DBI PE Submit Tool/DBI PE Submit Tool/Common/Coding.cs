44cdde57ec90b9da1bc88379ee9f0dd5a0784218
Minh Hiếu Phạm <pmh.ars14@gmail.com>
2019-02-14 11:19:07 +0700

    Encode - Decode

--- DBI PE Submit Tool/DBI PE Submit Tool/Common/Coding.cs
+++ DBI PE Submit Tool/DBI PE Submit Tool/Common/Coding.cs
@@ -4,23 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
+using EncryptData;
 
 namespace DBI_PE_Submit_Tool.Common
 {
     class Coding
     {
+        private static readonly string key = "12345678";
+
         public static string Encode(string json)
         {
-            // Encode here
-            string result = json;
-            return result;
+            var bytes = System.Text.Encoding.UTF8.GetBytes(json);
+            //var bytes = Convert.FromBase64String(json);
+
+            string encrypt = EncryptData.EncryptSupport.Encryption(bytes, key);
+            Console.WriteLine(encrypt);
+            return encrypt;
         }
 
         public static string Decode(string code)
         {
-            // Encode here
-            string json = code;
-            return json;
+            var bytes = System.Text.Encoding.UTF8.GetBytes(code);
+
+            string decrypt = EncryptData.EncryptSupport.Decryption(bytes, key);
+
+            return decrypt;
         }
     }
 }
