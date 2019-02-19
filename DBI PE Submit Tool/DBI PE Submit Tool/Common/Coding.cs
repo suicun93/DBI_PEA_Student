4d865d656cc737e700d39b989c8f180f4aa1efb4
duchtse04705 <duchtse04705@fpt.edu.vn>
2019-02-20 01:14:50 +0700

    Example for rsa

--- DBI PE Submit Tool/DBI PE Submit Tool/Common/Coding.cs
+++ DBI PE Submit Tool/DBI PE Submit Tool/Common/Coding.cs
@@ -5,31 +5,131 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using EncryptData;
+using System.IO;
+using DBI_PE_Submit_Tool.Model;
+using Newtonsoft.Json;
 
 namespace DBI_PE_Submit_Tool.Common
 {
     class Coding
     {
-        //private static readonly string key = "12345678";
-
-        public static string Encode(string json)
+        public static string Encryption(string strText)
         {
-            //var bytes = System.Text.Encoding.UTF8.GetBytes(json);
-            //string encrypt = EncryptData.EncryptSupport.Encryption(bytes, key);
-            //return encrypt;
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(json);
-            return System.Convert.ToBase64String(plainTextBytes);
+            var publicKey = "<RSAKeyValue>" +
+                "<Modulus>" +
+                "21wEnTU+mcD2w0Lfo1Gv4rtcSWsQJQTNa6gio05AOkV/Er9w3Y13Ddo5wGtjJ19402S71HUeN0vbKILLJdRSES5MHSdJPSVrOqdrll/vLXxDxWs/U0UT1c8u6k/Ogx9hTtZxYwoeYqdhDblof3E75d9n2F0Zvf6iTb4cI7j6fMs=" +
+                "</Modulus>" +
+                "<Exponent>AQAB</Exponent>" +
+                "</RSAKeyValue>";
+
+            var testData = Encoding.UTF8.GetBytes(strText);
+
+            using (var rsa = new RSACryptoServiceProvider(4096))
+            {
+                try
+                {
+                    // client encrypting data with public key issued by server                    
+                    rsa.FromXmlString(publicKey.ToString());
+
+                    var encryptedData = rsa.Encrypt(testData, true);
+
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+
+                    return base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
         }
 
-        public static string Decode(string code)
+        public static string Decryption(string strText)
         {
-            //var bytes = System.Text.Encoding.UTF8.GetBytes(code);
+            var privateKey = "<RSAKeyValue>" +
+                "<Modulus>" +
+                "21wEnTU+mcD2w0Lfo1Gv4rtcSWsQJQTNa6gio05AOkV/Er9w3Y13Ddo5wGtjJ19402S71HUeN0vbKILLJdRSES5MHSdJPSVrOqdrll/vLXxDxWs/U0UT1c8u6k/Ogx9hTtZxYwoeYqdhDblof3E75d9n2F0Zvf6iTb4cI7j6fMs=" +
+                "</Modulus>" +
+                "<Exponent>" +
+                "AQAB" +
+                "</Exponent>" +
+                "<P>/aULPE6jd5IkwtWXmReyMUhmI/nfwfkQSyl7tsg2PKdpcxk4mpPZUdEQhHQLvE84w2DhTyYkPHCtq/mMKE3MHw==" +
+                "</P>" +
+                "<Q>3WV46X9Arg2l9cxb67KVlNVXyCqc/w+LWt/tbhLJvV2xCF/0rWKPsBJ9MC6cquaqNPxWWEav8RAVbmmGrJt51Q==</Q>" +
+                "<DP>8TuZFgBMpBoQcGUoS2goB4st6aVq1FcG0hVgHhUI0GMAfYFNPmbDV3cY2IBt8Oj/uYJYhyhlaj5YTqmGTYbATQ==</DP>" +
+                "<DQ>FIoVbZQgrAUYIHWVEYi/187zFd7eMct/Yi7kGBImJStMATrluDAspGkStCWe4zwDDmdam1XzfKnBUzz3AYxrAQ==</DQ>" +
+                "<InverseQ>QPU3Tmt8nznSgYZ+5jUo9E0SfjiTu435ihANiHqqjasaUNvOHKumqzuBZ8NRtkUhS6dsOEb8A2ODvy7KswUxyA==</InverseQ>" +
+                "<D>cgoRoAUpSVfHMdYXW9nA3dfX75dIamZnwPtFHq80ttagbIe4ToYYCcyUz5NElhiNQSESgS5uCgNWqWXt5PnPu4XmCXx6utco1UVH8HGLahzbAnSy6Cj3iUIQ7Gj+9gQ7PkC434HTtHazmxVgIR5l56ZjoQ8yGNCPZnsdYEmhJWk=</D>" +
+                "</RSAKeyValue>";
 
-            //string decrypt = EncryptData.EncryptSupport.Decryption(bytes, key);
+            var testData = Encoding.UTF8.GetBytes(strText);
 
-            //return decrypt;
-            var base64EncodedBytes = System.Convert.FromBase64String(code);
-            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+            using (var rsa = new RSACryptoServiceProvider(4096))
+            {
+                try
+                {
+                    var base64Encrypted = strText;
+
+                    // server decrypting data with private key                    
+                    rsa.FromXmlString(privateKey);
+
+                    var resultBytes = Convert.FromBase64String(base64Encrypted);
+                    var decryptedBytes = rsa.Decrypt(resultBytes, true);
+                    var decryptedData = Encoding.UTF8.GetString(decryptedBytes);
+                    return decryptedData.ToString();
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+        string filePath = "";
+
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        /// <param name="file"> Path to file we want to save</param>
+        public Coding(string file)
+        {
+            filePath = file;
+        }
+
+        public void Save(Submition submition)
+        {
+            // Convert object to json
+            string json = JsonConvert.SerializeObject(submition);
+            // Spit json by 100
+            string[] arrayOfJson = Split(json, 100).ToArray();
+            // Encrypt all of it
+            List<string> hihi = new List<string>();
+            foreach (string item in arrayOfJson)
+            {
+                hihi.Add(Encryption(item));
+            }
+            // Write to file
+            File.WriteAllLines(filePath, hihi.ToArray());
+        }
+
+        public Submition Load()
+        {
+            // Read from file by line
+            string[] lines = File.ReadAllLines(filePath);
+            // Decrypt all of line and append it to json
+            string json = "";
+            foreach (string item in lines)
+            {
+                json = json + Decryption(item);
+            }
+            // Deconvert json to object
+            return JsonConvert.DeserializeObject<Submition>(json);
+        }
+
+        IEnumerable<string> Split(string str, int chunkSize)
+        {
+            return Enumerable.Range(0, str.Length / chunkSize)
+                .Select(i => str.Substring(i * chunkSize, chunkSize));
         }
     }
 }
---
+++ DBI PE Submit Tool/DBI PE Submit Tool/Common/Crypto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DBI_PE_Submit_Tool.Common
+{
+    class Crypto
+    {
+        
+    }
+}
