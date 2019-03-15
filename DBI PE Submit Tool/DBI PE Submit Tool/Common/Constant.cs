333092232d269ee9a9e28502edaa8e042900de8e
suicun93 <duchoang191@gmail.com>
2019-03-16 01:46:59 +0700

    Add server vao config

--- DBI PE Submit Tool/DBI PE Submit Tool/Common/Constant.cs
+++ DBI PE Submit Tool/DBI PE Submit Tool/Common/Constant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,6 @@ namespace DBI_PE_Submit_Tool.Common
 {
     class Constant
     {
-        public static string API_URL = "http://34.215.75.169";
-        //public static string API_URL = "http://localhost:8080";
+        public static string API_URL => ConfigurationManager.AppSettings["server"];
     }
 }
